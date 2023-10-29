d6fd340242313e5e35ae3b3cde0b312af98639e9
Shinjan-saha <shinjansaha02@gmail.com>
2023-10-29 20:38:12 +0530

    Added App for canteen website

---
+++ canapp/canapp/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:canapp/screen/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ canapp/canapp/lib/screen/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: WebViewPlus(
+          initialUrl: 'assets/index.html',
+        ),
+      ),
+    );
+  }
+}
