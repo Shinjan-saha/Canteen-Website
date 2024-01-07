b4824cb412b6dc51ba1130a3e680e716658120b3
Neha Sinha <nehasinha123987@gmail.com>
2024-01-08 03:12:44 +0530

    ..

--- Carousel.js
+++ Carousel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -30,36 +29,3 @@ let slideInterval = setInterval(function() {
     plusSlides(1); // Change the argument if you want to change the slide interval
   }, 2000);
   
-=======
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-
-}
-let slideInterval = setInterval(function() {
-    plusSlides(1); // Change the argument if you want to change the slide interval
-  }, 2000);
-  
->>>>>>> 92c513ecfdb120819d47d96284dc7936fe8e4bf6
